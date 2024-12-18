304a443829929c27c059ae7c41e96486fbe4098f
Bruno Lins Maia <bruno.maia@serpro.gov.br>
2024-12-18 15:42:38 -0300

    Tratamento redundancias, relacionamentos e otimizacao de consultas

--- src/main/java/br/com/alura/adopet/api/validations/ValidacaoPetComAdocaoEmAndamento.java
+++ src/main/java/br/com/alura/adopet/api/validations/ValidacaoPetComAdocaoEmAndamento.java
@@ -1,17 +1,11 @@
 package br.com.alura.adopet.api.validations;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.exception.ValidacaoException;
-import br.com.alura.adopet.api.model.Adocao;
-import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.model.StatusAdocao;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
-import br.com.alura.adopet.api.repository.PetRepository;
 
 @Component
 public class ValidacaoPetComAdocaoEmAndamento implements ValidacaoSolicitacaoAdocao {
@@ -19,17 +13,11 @@ public class ValidacaoPetComAdocaoEmAndamento implements ValidacaoSolicitacaoAdo
 	@Autowired
     private AdocaoRepository adocaoRepository;
 	
-	@Autowired
-    private PetRepository petRepository;
-	
 	public void validar(SolicitacaoAdocaoDto dto) {
-		
-		List<Adocao> adocoes = adocaoRepository.findAll();
-		Pet pet = petRepository.getReferenceById(dto.idPet());
-		for (Adocao a : adocoes) {
-            if (a.getPet() == pet && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
-            	throw new ValidacaoException("Pet já está aguardando avaliação para ser adotado!");
-            }
+		boolean petTemAdocaoEmAndamento = adocaoRepository.
+				existsByPetIdAndStatus(dto.idPet(), StatusAdocao.AGUARDANDO_AVALIACAO);
+		if (petTemAdocaoEmAndamento) {
+			throw new ValidacaoException("Pet já está aguardando avaliação para ser adotado!");
 		}
 	}
 }
